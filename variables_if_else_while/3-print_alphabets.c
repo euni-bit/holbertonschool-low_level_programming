960bf12d3af47efb33a4a050073bd78c3e34cbe4
eunielis5 <seunielis@gmail.com>
2023-10-16 14:34:05 -0400

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print alphabet
+* Return: always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	c = 'A';
+	while
+		(c <= 'Z') {
+		putchar (c);
+		c++;
+	}
+	return (0);
+}
