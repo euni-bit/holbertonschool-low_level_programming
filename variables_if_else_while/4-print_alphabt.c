dee7c1d96bfd66bfd8ba2245704b5bf142c880ee
eunielis5 <seunielis@gmail.com>
2023-10-17 09:07:10 -0400

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - print alphabet
+* Return: always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+		c++;
+	if (c != 'q' && c != 'e')
+	putchar(c);
+	c++;
+	}
+putchar('\n');
+return (0);
+}
