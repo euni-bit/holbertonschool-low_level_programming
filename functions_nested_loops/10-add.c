6df92797a65bd12c45de30a92cc22e475fe0481b
eunielis5 <seunielis@gmail.com>
2023-10-19 11:20:11 -0400

    finished code

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @num1: first interger
+ * @num2: second interger
+ *
+ * Return: return the result
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
