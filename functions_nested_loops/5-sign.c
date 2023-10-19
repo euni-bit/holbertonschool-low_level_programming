7efe7bbf5d3fff480f9cc4ba104d5061d95330d3
eunielis5 <seunielis@gmail.com>
2023-10-18 21:50:57 -0400

    pls be a correct output

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,19 +12,18 @@ int print_sign(int n)
 {
 	if  (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
-	putchar('\n');
 	return (0);
 }
