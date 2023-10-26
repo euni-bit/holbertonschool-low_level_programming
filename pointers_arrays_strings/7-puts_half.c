4828ada912eee90cf57cef2a8b141fd638d41922
eunielis5 <seunielis@gmail.com>
2023-10-26 09:32:29 -0400

    finished code

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int index = 0;
+	int len = 0;
+	int n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
