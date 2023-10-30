cae42c86476a02691c847f4d0fbbc0b361babdee
eunielis5 <seunielis@gmail.com>
2023-10-29 22:31:23 -0400

    finished code

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filed
+ * @b: constant byte to fill with
+ * @n: number of byte to fill
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
