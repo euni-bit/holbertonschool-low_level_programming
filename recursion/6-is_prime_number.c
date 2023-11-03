598599414f75e6bc906fb5cd1ad74cdbbb7ef103
eunielis5 <seunielis@gmail.com>
2023-11-03 10:54:37 -0400

    eliminated loop

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * is_prime_recursive - helper function to check for prime using recursion.
+ * @n: the interger to be checked
+ * @divisor: the current divisor to test
+ *
+ * Return: 1 if n is a prime number, 0 otherwise.
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_recursive(n, divisor + 1));
+}
+/**
  * is_prime_number - returns 1 if the input integer is a prime number.
  * @n: the integer to be checked.
  *
@@ -8,31 +34,5 @@
  */
 int is_prime_number(int n)
 {
-	int i = 5;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-
-	if (n <= 3)
-	{
-		return (1);
-	}
-
-	if ((n % 2 == 0) || (n % 3 == 0))
-	{
-		return (0);
-	}
-
-	while (i * i <= n)
-	{
-		if ((n % i == 0) || (n % (i + 2) == 0))
-		{
-			return (0);
-		}
-
-		i += 6;
-	}
-	return (1);
+	return (is_prime_recursive(n, 2));
 }
