b56b9a2260bb1020bd16e52a5ea224654e572a56
eunielis5 <seunielis@gmail.com>
2023-11-09 11:48:32 -0400

    typo in code

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[len1] != '\0')
 		len1++;
-	while (s1[len2] != '\0')
+	while (s2[len2] != '\0')
 		len2++;
 
 	concat_str = malloc((len1 + len2 + 1) * sizeof(char));
