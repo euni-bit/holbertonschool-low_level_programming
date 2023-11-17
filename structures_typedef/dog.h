1e2296205d46fb695cb47721ca5b6750fcaeda53
eunielis5 <seunielis@gmail.com>
2023-11-17 17:00:17 -0400

    finished task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,25 @@
 #define DOG_H
 
 /**
-* struct dog - a new type describing a dog.
-* @name: name of the dog
-* @age: age of the dog
-* @owner: owner of the dog
-*/
+ * struct dog - a new type describing a dog.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
