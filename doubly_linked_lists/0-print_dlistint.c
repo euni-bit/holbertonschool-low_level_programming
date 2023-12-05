0ab57d14d64ea69767a25ea0980ed30e9c83f479
eunielis5 <seunielis@gmail.com>
2023-12-05 13:16:41 -0400

    finish

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+/**
+ * print_dlistint - print elements of list
+ * @h: pointer to structure
+ * Return: length.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (1);
+
+}
