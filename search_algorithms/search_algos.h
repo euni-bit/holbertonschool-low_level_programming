a699ff30e3252bb34fd55b6fee943a20fdbee233
eunielis5 <seunielis@gmail.com>
2024-01-23 17:07:33 -0400

    fix typo

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -42,4 +42,8 @@ typedef struct skiplist_s
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
+
+
+#endif /* SEARCH_ALGOS_H */
